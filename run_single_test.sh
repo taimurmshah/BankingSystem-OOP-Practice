cf254af26540fc8a69e54d103103b66ed95e4666
Taimur Shah <taimurmshah@gmail.com>
2025-07-26 20:45:20 -0400

    tests

--- run_single_test.sh
+++ run_single_test.sh
@@ -1,2 +1,17 @@
-#!/bin/sh
-mocha specs -g "$1"
+#!/bin/bash
+
+# Check if test name argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: bash run_single_test.sh \"<test_case_name>\""
+    echo ""
+    echo "Example test names:"
+    echo "  \"Test level 1 case 01 basic create\""
+    echo "  \"Test level 1 case 02 basic create and deposit\""
+    echo "  \"level 1 case 01\""
+    echo ""
+    echo "Note: You can use partial matches - the script will find tests containing your search term"
+    exit 1
+fi
+
+echo "> npx mocha specs --colors -g \"$1\""
+npx mocha specs --colors -g "$1"
